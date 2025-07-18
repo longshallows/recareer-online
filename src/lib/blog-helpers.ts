9593837e27fded0118a189e6b4132333e0446c02
longshallows <nagase@longshallows.com>
2025-07-19 07:57:09 +0900

    記事関連のページを追加し、記事リンクのパスを更新

--- src/lib/blog-helpers.ts
+++ src/lib/blog-helpers.ts
@@ -130,11 +130,11 @@ export const getNavLink = (nav: string) => {
 }
 
 export const getPostLink = (slug: string) => {
-  return pathJoin(BASE_PATH, `/posts/${slug}`)
+  return pathJoin(BASE_PATH, `/articles/${slug}`)
 }
 
 export const getTagLink = (tag: string) => {
-  return pathJoin(BASE_PATH, `/posts/tag/${encodeURIComponent(tag)}`)
+  return pathJoin(BASE_PATH, `/articles/tag/${encodeURIComponent(tag)}`)
 }
 
 export const getPageLink = (page: number, tag: string) => {
@@ -144,9 +144,9 @@ export const getPageLink = (page: number, tag: string) => {
   return tag
     ? pathJoin(
         BASE_PATH,
-        `/posts/tag/${encodeURIComponent(tag)}/page/${page.toString()}`
+        `/articles/tag/${encodeURIComponent(tag)}/page/${page.toString()}`
       )
-    : pathJoin(BASE_PATH, `/posts/page/${page.toString()}`)
+    : pathJoin(BASE_PATH, `/articles/page/${page.toString()}`)
 }
 
 export const getDateStr = (date: string) => {
